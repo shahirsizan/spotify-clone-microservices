31d8212f2eb87634d279f5aac3111a7c6d28dde4
shahirsizan <shahir.sizan18@gmail.com>
2025-10-20 18:17:20 +0600

    loginUser and myProfile router & controller added

--- user_service/src/controllers.ts
+++ user_service/src/controllers.ts
@@ -44,3 +44,51 @@ export const registerUser = async (req: Request, res: Response) => {
 		});
 	}
 };
+
+export const loginUser = async (req: Request, res: Response) => {
+	try {
+		const { email, password } = req.body;
+
+		const user = await User.findOne({ email: email });
+
+		if (!user) {
+			res.status(404).json({
+				message: "User doesn't exist.",
+			});
+			return;
+		}
+
+		const passwordMatched = await bcrypt.compare(password, user.password);
+
+		if (!passwordMatched) {
+			res.status(400).json({
+				message: "Invalid Password",
+			});
+			return;
+		}
+
+		const token = jwt.sign(
+			{ _id: user._id },
+			process.env.JWT_SEC as string,
+			{
+				expiresIn: "3d",
+			}
+		);
+
+		res.status(200).json({
+			message: "Logged in",
+			user: user,
+			token: token,
+		});
+	} catch (error: any) {
+		res.status(500).json({
+			message: error.message,
+		});
+	}
+};
+
+export const myProfile = async (req: any, res: any) => {
+	const user = req.user;
+
+	res.json({ message: "Here is user object", user: user });
+};
--- user_service/src/routes.ts
+++ user_service/src/routes.ts
@@ -1,8 +1,11 @@
 import express from "express";
-import { registerUser } from "./controllers.js";
+import { loginUser, myProfile, registerUser } from "./controllers.js";
+import { isAuth } from "./middlewares.js";
 
 const router = express.Router();
 
 router.post("/user/register", registerUser);
+router.post("/user/login", loginUser);
+router.get("/user/me", isAuth, myProfile);
 
 export default router;
