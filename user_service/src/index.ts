7af6c685ef822932562b44ff0983574665b6051b
shahirsizan <shahir.sizan18@gmail.com>
2025-10-20 12:18:08 +0600

    mongodb connection string added

--- user_service/src/index.ts
+++ user_service/src/index.ts
@@ -2,13 +2,24 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-
 dotenv.config();
+import userRoutes from "./routes.js";
+
+const connectDb = async () => {
+	try {
+		mongoose.connect(process.env.MONGO_URI as string);
+
+		console.log("✅ MongoDB Connected");
+	} catch (error) {
+		console.log("❌ error -> connectDb(): ", error);
+	}
+};
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/v1", userRoutes);
 app.get("/", (req, res) => {
 	res.send("Server is working");
 });
@@ -16,7 +27,8 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 
 app.listen(5000, () => {
-	console.log(`Server is running on port ${port}`);
+	console.log(`✅ Server is running on port ${port}`);
+	connectDb();
 });
 
 /**
