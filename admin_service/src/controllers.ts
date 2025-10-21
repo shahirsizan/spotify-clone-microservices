ecc8c4c8d8eda8f3137474cdd6c5387b919a9554
shahirsizan <shahir.sizan18@gmail.com>
2025-10-21 18:51:46 +0600

    addSong() functionality added

--- admin_service/src/controllers.ts
+++ admin_service/src/controllers.ts
@@ -2,14 +2,15 @@ import cloudinary from "cloudinary";
 import getBuffer from "./config/dataUri.js";
 import { sql } from "./config/db.js";
 
-interface AuthencatedRequest extends Request {
+interface AuthenticatedRequest extends Request {
 	user?: {
 		_id: string;
 		role: string;
 	};
+	file?: any;
 }
 
-export const addAlbum = async (req: AuthencatedRequest, res: any) => {
+export const addAlbum = async (req: AuthenticatedRequest, res: any) => {
 	try {
 		if (req.user?.role !== "admin") {
 			res.status(401).json({
@@ -38,7 +39,7 @@ export const addAlbum = async (req: AuthencatedRequest, res: any) => {
 		}
 
 		const cloud = await cloudinary.v2.uploader.upload(fileBuffer.content, {
-			folder: "spotify",
+			folder: "Spotify/Albums",
 		});
 
 		// store the returned url for album-image in database
@@ -60,4 +61,58 @@ export const addAlbum = async (req: AuthencatedRequest, res: any) => {
 			message: error.message,
 		});
 	}
+};
+
+export const addSong = async (req: AuthenticatedRequest, res: any) => {
+	if (req.user?.role !== "admin") {
+		res.status(401).json({
+			message: "You are not admin",
+		});
+		return;
+	}
+
+	const { title, description, albumId } = req.body as any;
+
+	const isAlbum = await sql`SELECT * FROM albums WHERE id = ${albumId}`;
+	if (isAlbum.length === 0) {
+		res.status(404).json({
+			message: "No album with this id",
+		});
+		return;
+	}
+
+	const file = req.file;
+	if (!file) {
+		res.status(400).json({
+			message: "No file to upload",
+		});
+		return;
+	}
+
+	const fileBuffer = getBuffer(file);
+	if (!fileBuffer || !fileBuffer.content) {
+		res.status(500).json({
+			message: "Failed to generate file buffer",
+		});
+		return;
+	}
+
+	const cloud = await cloudinary.v2.uploader.upload(fileBuffer.content, {
+		folder: "Spotify/Songs",
+		resource_type: "video", // for both audio and video, doesn't matter
+	});
+
+	const result = await sql`
+    INSERT INTO songs (title, description, audio, album_id) VALUES
+    (${title}, ${description}, ${cloud.secure_url}, ${albumId})
+  `;
+
+	// if (redisClient.isReady) {
+	// 	await redisClient.del("songs");
+	// 	console.log("Cache invalidated for songs");
+	// }
+
+	res.json({
+		message: "Song Added",
+	});
 };
--- admin_service/src/routes.ts
+++ admin_service/src/routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import uploadFile, { isAuth } from "./middlewares.js";
-import { addAlbum } from "./controllers.js";
+import { addAlbum, addSong } from "./controllers.js";
 
 const router = express.Router();
 
 router.post("/album/new", isAuth, uploadFile, addAlbum);
+router.post("/song/new", isAuth, uploadFile, addSong);
 
 export default router;
