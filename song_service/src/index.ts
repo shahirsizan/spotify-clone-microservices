ece11e29bee7c7b105525e0591e06b0ec602d107
shahirsizan <shahir.sizan18@gmail.com>
2025-10-22 08:28:43 +0600

    routes boilerplate added

--- song_service/src/index.ts
+++ song_service/src/index.ts
@@ -2,10 +2,13 @@ import express from "express";
 import doetnv from "dotenv";
 import cors from "cors";
 doetnv.config();
+import songRoutes from "./routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1", songRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => {
