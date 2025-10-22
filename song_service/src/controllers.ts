7630d30c3c962f1d03f06ee9f0dbad521d3c5008
shahirsizan <shahir.sizan18@gmail.com>
2025-10-22 08:31:02 +0600

    song-services controller boilerplate added

---
+++ song_service/src/controllers.ts
@@ -0,0 +1,6 @@
+import { sql } from "./config/db.js";
+
+export const getAllAlbum = async () => {};
+export const getAllsong = async () => {};
+export const getAllSongsOfAlbum = async () => {};
+export const getSingleSong = async () => {};
