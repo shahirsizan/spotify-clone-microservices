94d40c6d81993aee46a295c9694b970f1d0792fe
shahirsizan <shahir.sizan18@gmail.com>
2025-10-22 12:01:21 +0600

    redisClient added

--- song_service/src/index.ts
+++ song_service/src/index.ts
@@ -3,14 +3,24 @@ import doetnv from "dotenv";
 import cors from "cors";
 doetnv.config();
 import songRoutes from "./routes.js";
+import { redisClient } from "./config/redis.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+redisClient
+	.connect()
+	.then(() => {
+		console.log("✅ Redis connceted");
+	})
+	.catch((error) => {
+		console.log("❌ Redis connection failed");
+	});
+
 app.use("/api/v1", songRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => {
-	console.log(`server is running on ${port}`);
+	console.log(`✅ server running on ${port}`);
 });
